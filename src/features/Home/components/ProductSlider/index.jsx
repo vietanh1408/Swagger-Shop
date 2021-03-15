21f0fb8cca1a1a74224aabdef721e857fe48a3e8
vietanh1408 <you@example.com>
2021-03-15 17:29:01 +0700

    update

--- src/features/Home/components/ProductSlider/index.jsx
+++ src/features/Home/components/ProductSlider/index.jsx
@@ -1,10 +1,10 @@
 import { Card, CardActions, CardContent, CardMedia, IconButton, makeStyles, Typography } from '@material-ui/core'
-import ShareIcon from '@material-ui/icons/Share'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Slider from 'react-slick'
+import { addToCart } from '../../../Cart/cartSlice'
 import { getProductsSlider } from '../../../Products/pathAPI'
 import './styles.scss'
 
@@ -47,8 +47,13 @@ const useStyles = makeStyles((theme) => ({
 function ProductSlider() {
 
     const productList = useSelector(state => state.product.list)
+    const [productIndex, setProductIndex] = useState(0)
     const dispatch = useDispatch()
+    const classes = useStyles();
 
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product))
+    }
     const NextArrow = ({ onClick }) => {
         return (
             <div className="arrow arrow-next" onClick={onClick}>
@@ -56,7 +61,6 @@ function ProductSlider() {
             </div>
         )
     }
-
     const PrevArrow = ({ onClick }) => {
         return (
             <div className="arrow arrow-prev" onClick={onClick}>
@@ -64,13 +68,6 @@ function ProductSlider() {
             </div>
         )
     }
-
-    useEffect(() => {
-        dispatch(getProductsSlider())
-    }, [])
-
-    const [productIndex, setProductIndex] = useState(0)
-
     const settings = {
         autoplay: false,
         infinite: true,
@@ -83,7 +80,9 @@ function ProductSlider() {
         prevArrow: <PrevArrow />,
         beforeChange: (current, next) => setProductIndex(next)
     }
-    const classes = useStyles();
+    useEffect(() => {
+        dispatch(getProductsSlider())
+    }, [])
 
     return (
         <div className="product-carousel">
@@ -105,7 +104,7 @@ function ProductSlider() {
                                 </Typography>
                             </CardContent>
                             <CardActions className={classes.action}>
-                                <IconButton aria-label="add to cart">
+                                <IconButton aria-label="add to cart" onClick={() => handleAddToCart(product)}>
                                     <ShoppingCartIcon />
                                 </IconButton>
                                 <Typography variant="body2">
